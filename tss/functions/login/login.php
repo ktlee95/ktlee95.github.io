6ce7eace33db8958066d8a2b35f76090c3fc0bc0
ktlee95 <tongkhlee@gmail.com>
2017-06-19 00:28:58 +0800

    testing database

--- tss/functions/login/login.php
+++ tss/functions/login/login.php
@@ -3,16 +3,7 @@
 		$user_name = $_POST['username'];
 		$user_password = $_POST['password'];
 		
-		<?php
-	//Defining the varible to connect the database
-	define('HOST','http://www.phpmyadmin.co');
-	define('USER','sql12180864');
-	define('PASS','MUuJ4pzGYS');
-	define('DB','sql12180864');
-	
-	//connect the database, if failed, output unable to connect
-	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect Database');
-?>
+		require_once('dbConnect.php');
 		require_once('encrypt_decrypt.php');
 		
 		define("ENCRYPTION_KEY", "ThErE_ARe_No_ENCrypTIon_KeY");
