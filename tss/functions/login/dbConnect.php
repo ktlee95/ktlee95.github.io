d1546bd624584022f5532b8b666da2037ea31a50
ktlee95 <tongkhlee@gmail.com>
2017-06-19 00:01:32 +0800

    update dbConnect file

--- tss/functions/login/dbConnect.php
+++ tss/functions/login/dbConnect.php
@@ -1,9 +1,9 @@
 <?php
 	//Defining the varible to connect the database
-	define('HOST','localhost');
-	define('USER','root');
-	define('PASS','');
-	define('DB','tss');
+	define('HOST','sql12.freemysqlhosting.net');
+	define('USER','sql12180864');
+	define('PASS','MUuJ4pzGYS');
+	define('DB','sql12180864');
 	
 	//connect the database, if failed, output unable to connect
 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect Database');
