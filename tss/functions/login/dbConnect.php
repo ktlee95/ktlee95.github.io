64b5a59f7a3d693d7884be95ca8abcd26b3b1902
ktlee95 <tongkhlee@gmail.com>
2017-06-19 10:27:12 +0800

    test database

--- tss/functions/login/dbConnect.php
+++ tss/functions/login/dbConnect.php
@@ -1,10 +1,51 @@
-<?php
-	//Defining the varible to connect the database
-	define('HOST','sql12.freemysqlhosting.net:3306');
-	define('USER','sql12180864');
-	define('PASS','MUuJ4pzGYS');
-	define('DB','sql12180864');
-	
-	//connect the database, if failed, output unable to connect
-	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect Database');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script>
+	var Client = require('mysql').Client;
+var client = new Client(); 
+client.host ='some.host.com';
+client.user = 'user';
+client.password = 'password';
+console.log("connecting...");
+client.connect(function(err, results) {
+    if (err) {
+        console.log("ERROR: " + err.message);
+        throw err;
+    }
+    console.log("connected.");
+    clientConnected(client);
+});
+
+clientConnected = function(client)
+{
+	tableHasData(client);
+}           
+
+
+tableHasData = function(client)
+{
+    client.query(
+        'SELECT * FROM db.table LIMIT 0,10',
+        function selectCb(err, results, fields) {
+            if (err) {
+                console.log("ERROR: " + err.message);
+                throw err;
+            }
+            console.log("Got "+results.length+" Rows:");
+            for(var i in results){
+			 
+				console.log(results[i].[column name]); 
+				console.log('\n');
+				
+            //console.log("The meta data about the columns:");
+            //console.log(fields);     
+			}
+            client.end();
+        });
+};
+	</script>
+</head>
+<body>
+</body>
+</html>
